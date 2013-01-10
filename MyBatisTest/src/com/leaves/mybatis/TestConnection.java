f89477b99a2b70d5678ca01b008dac9cc9dba743
WangZhenChong <WangZhenChong@WangZhenChong-PC>
2013-01-10 16:11:46 +0800

    Change-Id: Ie6454b3e61f9794b0fc5adf798225af567e5ceaf

--- MyBatisTest/src/com/leaves/mybatis/TestConnection.java
+++
@@ -1,9 +0,0 @@
-package com.leaves.mybatis;
-
-import java.io.IOException;
-
-public class TestConnection {
-	public static void main(String[] args) throws IOException {
-System.out.println("");
-	}
-}
