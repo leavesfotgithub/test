673b9d5e4e9412f524ee0b3faae96b17949ff7c5
leavesfotgithub <leavesforemail@gmail.com>
2013-01-10 16:29:08 +0800

    就是试验一下提交注释是怎么写的

--- TestGit/src/com/leaves/git/HelloGit.java
+++ TestGit/src/com/leaves/git/HelloGit.java
@@ -2,9 +2,6 @@ package com.leaves.git;
 
 public class HelloGit {
 	public static void main(String[] args) {
-		int i=5;
-		i=6;
-		i++;
-		System.out.println("你好git"+i);
+		System.out.println("你好git");
 	}
 }
